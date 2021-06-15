3a23f0e6a7781d12f91229eb2263cc23dbfbbd7f
Conrad Wahlen <conrad@vyer.com>
2021-06-15 16:37:21 +0200

    fix: createActionEffect should run when params update

--- src/createActionEffect.ts
+++ src/createActionEffect.ts
@@ -1,20 +1,23 @@
-import React, { useEffect, useRef } from 'react'
-import { ActionCreatorWithPayload } from './createAction'
+import { useEffect, Dispatch } from 'react'
+import {
+  ActionCreatorWithoutPayload,
+  ActionCreatorWithPayload,
+} from './createAction'
 import { AnyAction } from './redux-types'
+import { OptionalSpread } from './tsHelpers'
 
 export const createActionEffect = <Payload>(
-  action: ActionCreatorWithPayload<Payload>,
-  useDispatch: () => React.Dispatch<AnyAction>
-) =>
-  function useReturn(params: Payload) {
-    const dispatch = useDispatch()
-    const paramRef = useRef(params)
-    React.useEffect(() => {
-      paramRef.current = params
-    }, [params])
+  action: ActionCreatorWithPayload<Payload> | ActionCreatorWithoutPayload,
+  useDispatch: () => Dispatch<AnyAction>
+) => (...args: OptionalSpread<Payload>) => {
+  const dispatch = useDispatch()
+  const params = args[0]
 
-    useEffect(() => {
-      dispatch(action(paramRef.current))
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [paramRef])
-  }
+  useEffect(() => {
+    if (typeof params === 'undefined') {
+      dispatch((action as ActionCreatorWithoutPayload)())
+    } else {
+      dispatch((action as ActionCreatorWithPayload<Payload>)(params))
+    }
+  }, [dispatch, params])
+}
